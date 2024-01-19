8e5f51fa1fc93b89fab7589a8b6c6e42259439d2
AyaAhmed18 <yoyoahmed108@gmail.com>
2024-01-19 20:17:34 +0200

    testConsole

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Application.Service;
+using E_Commerce.Context;
+using E_Commerce.Infrustructure.Repository;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static IUserService _userService;
+        static void Main(string[] args)
+        {
+            _userService = new UserService(new UserRepository(new E_CommerceContext()));
+        }
+    }
+}
